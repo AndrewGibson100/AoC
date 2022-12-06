0c2e47cc4eaf6988a2ca189fcd80b4654b32146d
Andrew Gibson <andrew.gibson100@gmail.com>
2022-12-06 06:34:37 +0000

    day 6

---
+++ AoC/2022/Day6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC._2022
+{
+    internal class Day6
+    {
+        public static int Part1(string input) =>
+            GetResult(input, 4);
+
+        public static int Part2(string input) =>
+            GetResult(input, 14);
+
+        static int GetResult(string input, int uniqueCount) =>
+            input.Select((c, i) => new 
+            { 
+                Pos = i + uniqueCount, 
+                SubStr = input.Skip(i).Take(uniqueCount) 
+            })
+                .First(x => x.SubStr.Distinct().Count() == x.SubStr.Count())
+                .Pos;
+    }
+}
--- AoC/Program.cs
+++ AoC/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AoC._2022;
 using AoC._2022.Common;
-using System.Runtime.CompilerServices;
-using System.Text;
 
-var input = await Input.GetInput(5);
+var input = await Input.GetInput(6);
 
-Console.WriteLine(Day5.Part2(input));
+Console.WriteLine(Day6.Part2(input));
 Console.Read();
